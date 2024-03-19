bd7c6c69ad557351ac6d895c8c2a266cb0219828
nailahadayan <nailahadyan19@gmail.com>
2024-03-19 08:11:35 +0700

    tugas class

---
+++ app/src/main/java/com/example/kotlinarenta/aquarium3_2.kt
@@ -0,0 +1,23 @@
+package com.example.kotlinarenta
+
+class aquarium3_2 {
+    var width: Int = 20
+    var height: Int = 40
+    var length: Int = 100
+
+    fun printSize() {
+        println("Width: $width cm " +
+                "Length: $length cm " +
+                "Height: $height cm ")
+    }
+}
+fun buildAquarium3_2() {
+    val myAquarium = aquarium3_2()
+    myAquarium.printSize()
+    myAquarium.height = 60
+    myAquarium.printSize()
+}
+
+fun main() {
+    buildAquarium3_2()
+}
